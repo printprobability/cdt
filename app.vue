9350cb44256aded49c22d707597eda7a127ff3d3
jarmoza <jarmoza@gmail.com>
2024-02-21 12:19:33 -0500

    Default layout + Nuxt 3 component definitions

--- app.vue
+++ app.vue
@@ -1,7 +1,20 @@
 <template>
-    <v-app></v-app>
-        <div>
-            <NuxtWelcome/>
-        </div>
-    </v-app>
+    <div>
+        <NuxtLayout>
+            <NuxtPage/>
+        </NuxtLayout>
+    </div>
 </template>
+
+<script lang="ts">
+
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        
+        components: {
+            
+        }
+    });
+
+</script>
