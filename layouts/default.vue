32ac9b297bbc9bd79b0354175dd44167a53f8f87
jarmoza <jarmoza@gmail.com>
2024-02-21 12:22:15 -0500

    Default layout in transition for Nuxt 3

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app>
+
+        <v-app-bar>
+
+            <v-app-bar-title
+                class="dldt-navbar-title"
+                :text="$siteConfig.navbartitle"
+                :to="{ name: 'index' }"
+            ></v-app-bar-title>
+
+            <v-list-item class="dldt-navbar-link px-2">Books</v-list-item>
+            <v-list-item class="dldt-navbar-link px-2" :to="{ name: 'characters' }">Characters</v-list-item>
+            <v-list-item class="dldt-navbar-link px-2">Groupings</v-list-item>
+            <v-list-item class="dldt-navbar-link px-2">Data</v-list-item>
+            <v-list-item class="dldt-navbar-link pl-2 pr-3">About</v-list-item>
+
+            <!-- <v-list-item :to="{ name: 'books' }">Books</v-list-item>
+            <v-list-item :to="{ name: 'characters' }">Characters</v-list-item>
+            <v-list-item :to="{ name: 'groupings' }">Groupings</v-list-item>
+            <v-list-item :to="{ name: 'articles-slug', params: { slug: 'data' } }">Data</v-list-item>
+            <v-list-item :to="{ name: 'articles-slug', params: { slug: 'about' } }">About</v-list-item> -->
+        </v-app-bar>
+
+        <!-- NOTE: v-app is the Vuetify app tag and is required to use vuetify components without error -->
+        <v-main>
+            <v-container class="parent">
+
+                <!-- <v-app-bar class="shadow-sm mb-3">
+                    <v-app-bar-title :to="{ name: 'index' }" :text="$siteConfig.navbartitle"></v-app-bar-title>
+                    <v-divider></v-divider>
+                    <div class="d-flex ml-auto">
+                        <v-list-item>Books</v-list-item>
+                        <v-list-item>Characters</v-list-item>
+                        <v-list-item>Groupings</v-list-item>
+                        <v-list-item>Data</v-list-item>
+                        <v-list-item>About</v-list-item>
+                    </div>
+                </v-app-bar> -->
+
+                <AlphaAlert v-if="$siteConfig.alpha" :contact="$siteConfig.contact" />
+                <slot />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
