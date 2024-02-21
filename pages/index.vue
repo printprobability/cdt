6638dd52188bfc08ecbf2efc4094011e413f5f8b
jarmoza <jarmoza@gmail.com>
2024-02-21 12:22:35 -0500

    Home page changes for Nuxt3/Vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,49 @@
 <template>
+    
+    <v-container fluid>
 
-    <b-container>
-
+        <!-- Site title and subtitle -->
         <header class="text-center my-5">
-            <h1 class="display-2">{{ $config.maintitle }}</h1>
-            <h1 class="display-1">{{ $config.subtitle }}</h1>
-            <b-img :src="$config.fleuron.image" class="my-3" height="70" :alt="$config.fleuron.alt" />
-            <p class="display-2">{{ $config.abstract }}</p>
+            <h1 class="text-h2">{{ $siteConfig.maintitle }}</h1>
+            <h1 class="text-h1">{{ $siteConfig.subtitle }}</h1>
+            <img :src="$siteConfig.fleuron.image" class="my-3" height="70" :alt="$siteConfig.fleuron.alt" />
+            <p class="text-h3">{{ $siteConfig.abstract }}</p>
         </header>
 
-        <p class="display-3 text-center">Get started:</p>
+        <p class="text-h3 text-center">Get started:</p>
 
-        <b-row v-for="pair in paired_boxes.pairs" :key="pair.id" class="mb-2">
-            <b-col cols="6" v-for="box in pair.boxes" :key="box.image">
-                <SplashBox :image="box.image" :alt="box.alt" :to="box.to" :head="box.head" :lead="box.lead" />
-            </b-col>
-        </b-row>
+        <!-- Ways of browsing the site's contents and About -->
+            <v-row v-for="pair in paired_boxes.pairs" :key="pair.id" class="d-flex mb-2">
+                <!-- <v-col cols="2"></v-col> -->
+                <v-col cols="6" v-for="box in pair.boxes" :key="box.image">
+                    <SplashBox
+                        :alt="box.alt"
+                        :image="box.image"
+                        :title="box.title"
+                        :text="box.text"
+                        :to="box.to"/>
+                </v-col>
+                <!-- <v-col cols="2"></v-col> -->
+            </v-row>
 
-        <b-row v-if="!!paired_boxes.final_row" align-h="center">
-            <b-col cols="6">
-                <SplashBox :image="paired_boxes.final_row.image" :alt="paired_boxes.final_row.alt"
-                    :to="paired_boxes.final_row.to" :head="paired_boxes.final_row.head"
-                    :lead="paired_boxes.final_row.lead" />
-            </b-col>
-        </b-row>
+        <!--  Data download -->
+        <v-row v-if="!!paired_boxes.final_row" class="d-flex align-self-center">
+            <!-- <v-col cols="4"></v-col>  -->
+            <v-col cols="6">
+                <SplashBox
+                    :alt="paired_boxes.final_row.alt"
+                    :image="paired_boxes.final_row.image"
+                    :title="paired_boxes.final_row.title"
+                    :text="paired_boxes.final_row.text"
+                    :to="paired_boxes.final_row.to"/>
+            </v-col>
+            <!-- <v-col cols="4"></v-col> -->
+        </v-row>
 
-    </b-container>
-
+    </v-container>
 </template>
   
-<script lang="ts" setup>
-
-    import Vue from "vue";
-    // import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-    import "@/assets/scss/custom.scss";
-
-    const { $config } = useNuxtApp();
-
-    // Vue.use(BootstrapVue);
-    // Vue.use(IconsPlugin);
+<script>
 
     export default {
 
@@ -48,11 +53,11 @@
 
             return {
 
-                config: Vue.config,
                 paired_boxes: {
-                    pairs: [],
+                    
                     final_row: null,
-                },
+                    pairs: []
+                }
             };
         },
 
@@ -60,23 +65,30 @@
 
             // Split splashboxes into pairs of two, with an optional final singleton if list is odd-numbered
             var index = 0;
-            const n_boxes = this.$config.splashboxes.length;
-            while (index <= n_boxes - 2) {
+            const n_boxes = this.$siteConfig.splashboxes.length;
+            while ( index <= n_boxes - 2 ) {
+
                 this.paired_boxes.pairs.push({
+
                     id: index,
                     boxes: [
-                        this.$config.splashboxes[index],
-                        this.$config.splashboxes[index + 1],
+
+                        this.$siteConfig.splashboxes[index],
+                        this.$siteConfig.splashboxes[index + 1],
                     ],
                 });
+
                 index += 2;
             }
+
             // Add on final single row if needed
-            if (index == n_boxes - 1) {
-                this.paired_boxes.final_row = this.$config.splashboxes[index];
+            if ( index == n_boxes - 1 ) {
+
+                this.paired_boxes.final_row = this.$siteConfig.splashboxes[index];
             }
         }
-    });
+    };
+
 </script>
 
 <style></style>
