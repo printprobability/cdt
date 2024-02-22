4e2a0967b38cef8d17d3ecef110ae9d1cd937482
jarmoza <jarmoza@gmail.com>
2024-02-22 12:48:22 -0500

    Date slider for book pages

---
+++ client-components/DateSlider.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <ClientOnly>
+        <vue-slider
+            ref="slider"
+            id="input-book-date"
+            :data-label="Date"
+            :enable-cross="false"
+            :min="1600"
+            :max="1800"
+            :value="value"
+            @change="$emit('input', $event)"
+            tooltip="always"
+            tooltipPlacement="bottom" />
+    </ClientOnly>
+
+</template>
+  
+<script>
+
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
+export default {
+
+    components: {
+    
+        VueSlider
+    },
+
+    props: {
+
+        value: Array,
+    }
+};
+
+</script>
+  
