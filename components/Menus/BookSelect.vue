96859d4e9f7dcfc7c9520e4f1ab7a478b1ea169c
jarmoza <jarmoza@gmail.com>
2024-02-22 15:33:59 -0500

    Book select dropdown

---
+++ components/Menus/BookSelect.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <v-select
+        id="input-character-book"
+        :debounce="500"
+        :options="all_books"
+        :value="value"
+        @input="$emit('input', $event)"
+        label="Book" />
+
+</template>
+  
+<script>
+
+const ANY_BOOK = {
+    
+    text: "any",
+    value: "any"
+};
+
+export default {
+
+    props: {
+
+        value: {
+
+            type: String,
+            default: "any",
+        }
+    },
+
+    data() {
+
+        return {
+
+            all_books: []
+        };
+    },
+    async fetch() {
+
+        const fetched_books = await this.$content("books")
+            .sortBy("pqTitle")
+            .only(["id", "label"])
+            .fetch();
+
+        this.all_books = fetched_books.map((book) => {
+
+            return {
+
+                text: book.label,
+                value: book.id,
+            };
+        });
+        this.all_books.unshift(ANY_BOOK);
+    },
+
+    created() {
+
+        this.$fetch();
+    }
+};
+
+</script>
