6c65abf62bf6c49a52884829a6172cfe9727d26f
jarmoza <jarmoza@gmail.com>
2024-02-22 15:33:09 -0500

    Page image component

---
+++ components/PageImage.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="media-box">
+        <v-img class="page-cover" :src="page.image.thumbnail" :alt="page.label" />
+    </div>
+
+</template>
+  
+<script>
+
+export default {
+
+    props: {
+
+        page: Object,
+    },
+};
+
+</script>
+
+<style scoped>
+
+.media-box {
+    
+    width: 215px;
+}
+
+.page-cover {
+
+    max-width: 200px;
+    max-height: 275px;
+}
+
+</style>
+  
