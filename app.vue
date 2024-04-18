eb234b142fcaab64c8f2d874375158658672bdf9
jarmoza <jarmoza@gmail.com>
2024-04-18 17:33:08 -0400

    Config updates

--- app.vue
+++ app.vue
@@ -5,16 +5,3 @@
         </NuxtLayout>
     </div>
 </template>
-
-<script lang="ts">
-
-    import { defineComponent } from "vue";
-
-    export default defineComponent({
-        
-        components: {
-            
-        }
-    });
-
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,93 @@
 // Imports
-
+import * as cdt_routes from "./assets/json/cdt_routes.json"
+import { resolve } from "path";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+// const getStaticRoutes = async () => {
+
+//     const characters = await queryContent("characters").only(["id"]).find();
+//     const books = await queryContent("books").only(["id"]).find();
+//     const groupings = await queryContent("groupings").only(["id"]).find();
+
+//     const character_routes = characters.map(x => "/characters/" + x.id);
+//     const book_routes = books.map(x => "/books/" + x.id);
+//     const grouping_routes = groupings.map(x => "/groupings/" + x.id);
+
+//     return character_routes.concat(book_routes, grouping_routes);    
+// };
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-    build: {
+    // alias: { "@": resolve(__dirname, "/") },
 
-        transpile: ["vuetify"],
-    },
-
-    css: [
+    build: { transpile: ["vuetify"] },
     
-        "@/assets/scss/custom.scss"
-    ],
+    content: {
+    
+        // fullTextSearchFields: ["label", "notes"],
+        // nestedProperties: ["page.sequence"]
+        experimental: { search: true }
+    },    
 
-    devtools: {
+    css: [ "~/assets/scss/custom.scss" ],
 
-        enabled: false
-    },
+    devtools: { enabled: true },
+
+    hooks: {
+        
+        async "nitro:config"(nitroConfig) {
+
+            // Fetch the routes from our function above
+            // const slugs = await getStaticRoutes();
+
+            const character_ids = cdt_routes.characters;
+            const book_ids = cdt_routes.books;
+            const grouping_ids = cdt_routes.groupings;
+            
+            const character_routes = character_ids.map(x => "/characters/" + x.id);
+            const book_routes = book_ids.map(x => "/books/" + x.id);
+            const grouping_routes = grouping_ids.map(x => "/groupings/" + x.id);
+
+            const slugs = character_routes.concat(book_routes, grouping_routes);
+
+            // Add the characters, books, and groupings routes to the nitro config routes list
+            nitroConfig.prerender.routes.push(...slugs);
+        }
+    },    
+
+    // hooks: {
+
+    //     async "nitro:config" (nitroConfig) {
+          
+    //         if ( nitroConfig.dev ) {
+
+    //             return;
+    //         }
+            
+    //         // Async logic
+    //         const characters = await queryContent("characters").only(["id"]).find();
+    //         const books = await queryContent("books").only(["id"]).find();
+    //         const groupings = await queryContent("groupings").only(["id"]).find();
+      
+    //         const character_routes = characters.map(x => "/characters/" + x.id);
+    //         const book_routes = books.map(x => "/books/" + x.id);
+    //         const grouping_routes = groupings.map(x => "/groupings/" + x.id);
+
+    //         console.log("CHARACTER ROUTES");
+    //         console.log(JSON.stringify(character_routes));
+      
+    //         // return character_routes.concat(book_routes, grouping_routes);
+            
+    //         nitroConfig.prerender.routes.push(character_routes.concat(book_routes, grouping_routes));
+    //     }
+    // },    
 
     modules: [
+
+        "@nuxt/content",
+        
+        "@vueuse/nuxt",
 
         (_options, nuxt) => {
 
@@ -29,10 +96,22 @@ export default defineNuxtConfig({
                 // @ts-expect-error
                 config.plugins.push(vuetify({ autoImport: true }));
             });
-        },
+        }        
+    ],    
 
-        "@nuxt/content"
-    ],  
+    // optimizeDeps: {
+        
+    //     exclude: [
+            
+    //         "@nuxt/content",
+    //         "@vueuse/core",
+    //         "@vueuse/nuxt"
+    //     ]
+    // },
+
+    sourcemap: true,
+
+    ssr: true,
 
     vite: {
 
