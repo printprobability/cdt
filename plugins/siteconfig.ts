4e1a2b93d5fb454ac29636a5929bbdffd396fa60
jarmoza <jarmoza@gmail.com>
2024-04-18 17:25:57 -0400

    Plugin updates + new error handler

--- plugins/siteconfig.ts
+++ plugins/siteconfig.ts
@@ -1,4 +1,4 @@
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
     
     return {
         
@@ -6,66 +6,84 @@ export default defineNuxtPlugin((nuxtApp) => {
 
             siteConfig: {
 
-                maintitle: "Catalog of Distinctive Type",
-                subtitle: "Restoration England",
                 abstract: "A curated reference for distinctive type pieces in early English printed books",
-                navbartitle: "CDT - Restoration England",
-                contact: {
 
-                    fullname: "Jonathan Armoza",
-                    email: "jarmoza@andrew.cmu.edu",
-                    url: "http://printprobability.org/"
-                },
                 alpha: true,
-                github: "https://github.com/printprobability/cdt",
-                fleuron: {
-                    image: "/img/fleuron.svg",
-                    alt: "Fleuron"
-                },
+
                 browsecopy: {
 
                     books: "Search books indexed by this project based on title, printer, and date.",
                     characters: "Search for characters by type or source book.",
                     groupings: "Curated sets of characters identified by a human & machine-learning pipeline that are useful for identifying certain printers or relationships between them."
+                },                
+
+                contact: {
+
+                    email: "jarmoza@andrew.cmu.edu",
+                    fullname: "Jonathan Armoza",
+                    url: "http://printprobability.org/"
                 },
+
+                fleuron: {
+
+                    alt: "Fleuron",
+                    image: "/img/fleuron.svg"
+                },
+
+                github: "https://github.com/printprobability/cdt",
+
+                maintitle: "Catalog of Distinctive Type",
+
+                navbartitle: "CDT - Restoration England",
+
                 splashboxes: [
 
                     {
-                        title: "Browse by book",
-                        text: "Start from a particular book to see the distinctive character types we have identified in it",
-                        image: "/img/a.jpg",
                         alt: "Illustrated A character",
+                        image: "/img/a.jpg",
+                        text: "Start from a particular book to see the distinctive character types we have identified in it",
+                        title: "Browse by book",
                         to: { name: "books" }
                     },
                     {
-                        title: "Browse by character",
-                        text: "Search thousands of characters by glyph and damage types",
-                        image: "/img/i.jpg",
                         alt: "Ornate I character",
+                        image: "/img/i.jpg",
+                        text: "Search thousands of characters by glyph and damage types",
+                        title: "Browse by character",
+                        to: { name: "" }
+                    },
+
+                    /*{
+                        alt: "Ornate I character",
+                        image: "/img/i.jpg",
+                        text: "Search thousands of characters by glyph and damage types",
+                        title: "Browse by character",
                         to: { name: "characters" }
                     },
                     {
-                        title: "Browse by curated groupings",
-                        text: "Explore curated sets of distinctive characters that have been identified by our project team.",
-                        image: "/img/i.jpg",
                         alt: "Ornate I character",
+                        image: "/img/i.jpg",
+                        text: "Explore curated sets of distinctive characters that have been identified by our project team.",
+                        title: "Browse by curated groupings",
                         to: { name: "groupings" }
                     },
-                    /*{
-                        title: "Learn about the project",
-                        text: "Learn about the motivation and methodology of the \"Dictionary\", meet the project team, and find associated publications and machine-readable data",
-                        image: "/img/s.jpg",
+                    {
                         alt: "embellished capital S",
-                        to: { name: 'articles-slug', params: { slug: 'about' } }
+                        image: "/img/s.jpg",
+                        text: "Learn about the motivation and methodology of the \"Dictionary\", meet the project team, and find associated publications and machine-readable data",
+                        title: "Learn about the project",
+                        to: { name: "articles", params: { slug: "about" } }
                     },
                     {
-                        title: "Access data",
-                        text: "Download machine-readable data on books and segmented characters",
-                        image: "/img/m.jpg",
                         alt: "Ornate M",
-                        to: { name: "articles-slug", params: { slug: "data" } }
+                        image: "/img/m.jpg",
+                        text: "Download machine-readable data on books and segmented characters",
+                        title: "Access data",
+                        to: { name: "articles", params: { slug: "data" } }
                     }*/
-                ]
+                ],
+
+                subtitle: "Restoration England"
             }
         }
     };
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,6 @@
 // Imports
 
 import "@mdi/font/css/materialdesignicons.css";
-
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
