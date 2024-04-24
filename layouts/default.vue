30120567ec9c88d1009fdcc37f43823db0d62a61
jarmoza <jarmoza@gmail.com>
2024-04-24 17:26:05 -0400

    Re-enabling characters in the nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
             ></v-app-bar-title>
 
             <v-list-item class="dldt-navbar-link px-2" :to="{ name: 'books' }">Books</v-list-item>
-            <v-list-item class="dldt-navbar-link px-2">Characters</v-list-item>
+            <v-list-item class="dldt-navbar-link px-2" :to="{ name: 'characters' }">Characters</v-list-item>
             <v-list-item class="dldt-navbar-link px-2">Groupings</v-list-item>
             <v-list-item class="dldt-navbar-link px-2">Data</v-list-item>
             <v-list-item class="dldt-navbar-link pl-2 pr-3">About</v-list-item>
