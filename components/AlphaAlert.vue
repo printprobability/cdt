f5adb850be187e888ace7b6c7bf501de34cd3f7f
jarmoza <jarmoza@gmail.com>
2024-04-29 10:33:12 -0400

    Alert cleanup and style move to local

--- components/AlphaAlert.vue
+++ components/AlphaAlert.vue
@@ -1,6 +1,7 @@
 <template>
 
-    <v-alert show type="error" class="dldt-alert">
+    <!-- <v-alert show type="error" class="dldt-alert"> -->
+    <v-alert class="dldt-alert" show>        
         {{ alertText }}
         Contact
         <a :href="`mailto:${$siteConfig.contact.email}`">
@@ -18,3 +19,17 @@
     const alertText = ref("This is an alpha site. Content and style are subject to change at anytime.");
 
 </script>
+
+<style>
+
+.dldt-alert {
+
+    background-color: #f8d7da !important;
+    border-color: #f5c6cb !important;
+    border-radius: 0 !important;
+    color: #721c24 !important;
+    font-family: $font-family-base;
+    font-size: 1rem;
+}
+
+</style>
