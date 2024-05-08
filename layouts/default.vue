a13b25bfed161e4c90469f9cc15d9d1b5a8c0688
jarmoza <jarmoza@gmail.com>
2024-05-08 10:30:14 -0400

    Enabling groupings and making title hyperlink in app bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,20 @@
 
         <v-app-bar class="shadow-sm">
 
-            <v-app-bar-title
+            <!-- <v-app-bar-title
                 class="dldt-navbar-title"
                 :text="$siteConfig.navbartitle"
                 :to="{ name: 'index' }"
-            ></v-app-bar-title>
+            > -->
+            <v-app-bar-title class="dldt-navbar-title">            
+                <NuxtLink to="/" style="text-decoration: none;">
+                    {{ $siteConfig.navbartitle }}
+                </NuxtLink>
+            </v-app-bar-title>
 
             <v-list-item class="dldt-navbar-link px-2" :to="{ name: 'books' }">Books</v-list-item>
             <v-list-item class="dldt-navbar-link px-2" :to="{ name: 'characters' }">Characters</v-list-item>
-            <v-list-item class="dldt-navbar-link px-2">Groupings</v-list-item>
+            <v-list-item class="dldt-navbar-link px-2" :to="{ name: 'groupings' }">Groupings</v-list-item>
             <v-list-item class="dldt-navbar-link px-2">Data</v-list-item>
             <v-list-item class="dldt-navbar-link pl-2 pr-3">About</v-list-item>
 
