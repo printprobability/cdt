f04bbc070d0050864c43f5a89e4f17f45a268c6e
jarmoza <jarmoza@gmail.com>
2024-05-13 12:50:37 -0400

    Cleanup, removing filtered characters prop

--- components/GroupingCard.vue
+++ components/GroupingCard.vue
@@ -8,7 +8,7 @@
 
         <div class="d-flex flex-wrap">
 
-            <CharacterImage v-for="character in filteredCharacters" :key="character.id" :character="character"
+            <CharacterImage  v-for="character in characters" :key="character.id" :character="character"
                 :selected="isSelectedCharacter(character.id)" />
 
             <NuxtLink :to="groupingLink">
@@ -26,37 +26,27 @@
 
     const props = defineProps({
 
-        filteredCharacters: Array,
         grouping: Object,
         selected_character_id: String
     });
     
     // Data
 
-    var filteredCharacters = ref([]);
-    if ( props.filteredCharacters ) {
-
-        filteredCharacters.value = props.filteredCharacters;
-    } else {
-
-        ({ data: filteredCharacters } = await useAsyncData("myCharacters", () => queryContent("characters")
-        // .only(["id", "label", "image"])    
+    const { data: characters } = await useAsyncData(`myCharacters${props.grouping.id}`, () => queryContent("characters")
+        .only(["id", "label", "image"])    
         .where({ id: { $in: props.grouping.characters }})
-        .sort({"characterClass": 1})
+        //.sort({"characterClass": 1})
         .limit(10)
         .find()
-    ));        
-    }
+    );
 
     // Methods
-
     function isSelectedCharacter(p_charID) {
 
         return p_charID === props.selected_character_id;
     }
 
     // Computed
-
     const groupingLink = computed(() => {
 
         return { name: "groupings-id", params: { id: props.grouping.id } };
