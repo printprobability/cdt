ca58555f28144d746b926bf01e4af02d0c88a18e
jarmoza <jarmoza@gmail.com>
2024-05-18 05:57:44 -0400

    Continued work on image download portion of ingestion script

--- ingest_groupings.py
+++ ingest_groupings.py
@@ -11,9 +11,12 @@ import argparse
 import json
 import os
 import requests
+import shutil
 import sys
 from uuid import UUID
 
+# Third party
+from tqdm import tqdm
 
 # Globals
 
@@ -27,17 +30,19 @@ API_URLS = {
 CERTIFICATE_FILE_PATH = "{0}{1}ingest{1}bridges_files{1}server.crt".format(os.getcwd(), os.sep)
 DEFAULT_JSON_OUTPUT_DIRECTORY = "{0}{1}content{1}".format(os.getcwd(), os.sep)
 DEFAULT_IMAGE_OUTPUT_DIRECTORY = "{0}{1}public{1}".format(os.getcwd(), os.sep)
+WORKBENCH_URL = "https://printprobdb.psc.edu/"
 
 
 # Classes
 
 class API_Object:
 
-    def __init__(self, p_grouping_id, p_api_url, p_object_txt_name):
+    def __init__(self, p_id, p_api_url, p_object_txt_name):
 
-        self.m_id = p_grouping_id
+        self.m_id = p_id
         self.m_object_txt_name = p_object_txt_name
-        self.m_api_object = API_Object.get_api_object(p_api_url, p_grouping_id, p_object_txt_name)
+        self.m_api_object = API_Object.get_api_object(p_api_url, p_id, p_object_txt_name)
+        self.m_json_object = None
 
     @property
     def api_object(self):
@@ -46,20 +51,26 @@ class API_Object:
     def id(self):
         return self.m_id
     @property
+    def json_object(self):
+        return self.m_json_object
+    @property
     def object_name(self):
         return self.m_object_txt_name
+
+    def make_image_paths(self):
+        pass
 
     def output_to_file(self, p_output_directory):
         with open(f"{p_output_directory}{self.m_object_txt_name}_{self.m_id}.json", "w") as output_file:
             json.dump(self.m_api_object, output_file)
 
-    def to_json(self):
-        return None       
+    def save_as_json(self):
+        pass      
 
     @staticmethod
     def get_api_object(p_api_url, p_uuid, p_object_name):
 
-        print(f"Retrieving data for {p_object_name}: {p_uuid}...")
+        # print(f"Retrieving data for {p_object_name}: {p_uuid}...")
 
         try:
             r = requests.get(
@@ -73,6 +84,32 @@ class API_Object:
 
             print(f"Error fetching existing {p_object_name} for {p_uuid}: {err}")
             exit(0)
+
+    @staticmethod
+    def get_workbench_image(p_url, p_prefix_to_replace, p_output_path):
+
+        # ex. https://printprobdb.psc.edu/iiif/books/british_library/asowle_xxxxx_yyyyy_00height_heathen/lines_color/asowle_xxxxx_yyyyy_00height_heathen-0001_page1r.tif/full/full/0/default.jpg
+
+        formatted_url = p_url.replace(p_prefix_to_replace, WORKBENCH_URL)
+        disk_output_path = p_output_path + p_url.strip(os.sep)
+
+        # 1. Download the image
+        try:
+            print(f"Downloading {formatted_url} ...")
+            r = requests.get(formatted_url)
+        except requests.exceptions.HTTPError as err:
+            print(f"Error fetching image {formatted_url}: {err}")
+            return
+
+        print(f"Writing file to: {disk_output_path}")
+
+        # 2. Write the response to a file in the appropriate location
+        with open(disk_output_path, "wb") as output_file:
+            output_file.write(r.content)
+
+    @staticmethod
+    def localize_url(p_url):
+        return p_url
 
     @staticmethod
     def is_valid_uuid(p_uuid_to_test, p_version=4):
@@ -108,13 +145,41 @@ class Book(API_Object):
     s_api_url = API_URLS["BOOKS"]
     s_object_txt_name = "book"
     
-    def __init__(self, p_grouping_id):
+    def __init__(self, p_book_id):
 
-        super().__init__(p_grouping_id, Book.s_api_url, Book.s_object_txt_name)
-        
-    def to_json(self):
+        super().__init__(p_book_id, Book.s_api_url, Book.s_object_txt_name)
+        self.save_as_json()
+    
+    def make_image_paths(self, p_image_output_directory):
 
-        return {
+        image_paths = {
+
+            # 'IIIF base' path
+            # (ex. /img/iiif/books/british_library/asowle_xxxxx_yyyyy_00height_heathen/lines_color/asowle_xxxxx_yyyyy_00height_heathen-0001_page1r.tif)
+            # "iiifBase": self.m_json_object["coverPage"]["image"]["iiifBase"],
+
+            # 'Web URL' path
+            # "webUrl": self.m_json_object["coverPage"]["image"]["webUrl"],
+
+            # 'Thumbnail' path
+            "coverPage.image.thumbnail": self.m_json_object["coverPage"]["image"]["thumbnail"]
+        }
+
+        for path_key in image_paths:
+
+            if os.path.exists(f"{p_image_output_directory}{image_paths[path_key]}"):
+                # print(f"{p_image_output_directory}{image_paths[path_key].strip(os.sep)} already exists. Skipping...")
+                continue
+
+            # NOTE: This will also create the parent path (i.e. /img/iiif/books/) if it does not yet exist in the output directory
+            try:
+                os.makedirs(p_image_output_directory + os.path.dirname(image_paths[path_key].strip(os.sep)))
+            except FileExistsError as err:
+                continue
+
+    def save_as_json(self):
+
+        self.m_json_object = {
 
             "id": self.m_api_object["id"],
             "eebo": self.m_api_object["eebo"],
@@ -140,10 +205,10 @@ class Book(API_Object):
                 "image": {
                     
                     "tif": self.m_api_object["cover_page"]["image"]["tif"],
-                    "iiifBase": self.m_api_object["cover_page"]["image"]["iiif_base"],
-                    "webUrl": self.m_api_object["cover_page"]["image"]["web_url"],
-                    "thumbnail": self.m_api_object["cover_page"]["image"]["thumbnail"],
-                    "fullTif": self.m_api_object["cover_page"]["image"]["full_tif"]
+                    "iiifBase": self.m_api_object["cover_page"]["image"]["iiif_base"].replace(WORKBENCH_URL, "/img/"),
+                    "webUrl": self.m_api_object["cover_page"]["image"]["web_url"].replace(WORKBENCH_URL, "/img/"),
+                    "thumbnail": self.m_api_object["cover_page"]["image"]["thumbnail"].replace(WORKBENCH_URL, "/img/"),
+                    "fullTif": self.m_api_object["cover_page"]["image"]["full_tif"].replace(WORKBENCH_URL, "/img/")
                 }
             },
             "isEeboBook": self.m_api_object["is_eebo_book"],
@@ -159,17 +224,51 @@ class Character(API_Object):
     s_api_url = API_URLS["CHARACTERS"]
     s_object_txt_name = "character"
     
-    def __init__(self, p_grouping_id):
+    def __init__(self, p_character_id):
 
-        super().__init__(p_grouping_id, Character.s_api_url, Character.s_object_txt_name)
+        super().__init__(p_character_id, Character.s_api_url, Character.s_object_txt_name)
+        self.save_as_json()
 
     @property
     def book(self):
         return self.m_api_object["book"]["id"]
-        
-    def to_json(self):
 
-        return {
+    def make_image_paths(self, p_image_output_directory):
+
+        image_paths = {
+
+            # 'Buffer' path
+            # (ex. /img/iiif/books/restoration/anon_R5466_usnnnc_menetekel1663/lines_color/anon_R5466_usnnnc_menetekel1663-0005_page1r.tif/1477,1055,179,169/150,/0/default.jpg)
+            "image.buffer": self.m_json_object["image"]["buffer"],
+
+            # 'Web URL' path
+            "image.webUrl": self.m_json_object["image"]["webUrl"],
+
+            # 'Thumbnail' path
+            "page.image.thumbnail": self.m_json_object["page"]["image"]["thumbnail"],
+
+            # 'IIIF base' path
+            "page.image.iiifBase": self.m_json_object["page"]["image"]["iiifBase"],
+
+            # 'Page image webUrl' path
+            # "page.image.webUrl": self.m_json_object["page"]["image"]["webUrl"]            
+        }
+
+        for path_key in image_paths:
+
+            if os.path.exists(f"{p_image_output_directory}{image_paths[path_key]}"):
+                # print(f"{p_image_output_directory}{image_paths[path_key].strip(os.sep)} already exists. Skipping...")
+                continue
+
+            # NOTE: This will also create the parent path (i.e. /img/iiif/books/) if it does not yet exist in the output directory
+            try:
+                os.makedirs(p_image_output_directory + os.path.dirname(image_paths[path_key].strip(os.sep)))
+            except FileExistsError as err:
+                continue
+        
+    def save_as_json(self):
+
+        self.m_json_object = {
 
             "id": self.m_api_object["id"],
             "label": self.m_api_object["label"],
@@ -186,17 +285,17 @@ class Character(API_Object):
                 "image": {
 
                     "tif": self.m_api_object["page"]["image"]["tif"],
-                    "iiifBase": self.m_api_object["page"]["image"]["iiif_base"],
-                    "webUrl": self.m_api_object["page"]["image"]["web_url"],
-                    "thumbnail": self.m_api_object["page"]["image"]["thumbnail"],
-                    "fullTif": self.m_api_object["page"]["image"]["full_tif"]
+                    "iiifBase": self.m_api_object["page"]["image"]["iiif_base"].replace(WORKBENCH_URL, "/img/"),
+                    "webUrl": self.m_api_object["page"]["image"]["web_url"].replace(WORKBENCH_URL, "/img/"),
+                    "thumbnail": self.m_api_object["page"]["image"]["thumbnail"].replace(WORKBENCH_URL, "/img/"),
+                    "fullTif": self.m_api_object["page"]["image"]["full_tif"].replace(WORKBENCH_URL, "/img/")
                 }
             },
             "image": {
 
-                "webUrl": self.m_api_object["image"]["web_url"],
-                "thumbnail": self.m_api_object["image"]["thumbnail"],
-                "buffer": self.m_api_object["image"]["buffer"]
+                "webUrl": self.m_api_object["image"]["web_url"].replace(WORKBENCH_URL, "/img/"),
+                "thumbnail": self.m_api_object["image"]["thumbnail"].replace(WORKBENCH_URL, "/img/"),
+                "buffer": self.m_api_object["image"]["buffer"].replace(WORKBENCH_URL, "/img/")
             },
             "absoluteCoords": {
 
@@ -216,14 +315,15 @@ class Grouping(API_Object):
     def __init__(self, p_grouping_id):
 
         super().__init__(p_grouping_id, Grouping.s_api_url, Grouping.s_object_txt_name)
+        self.save_as_json()
 
     @property
     def characters(self):
         return [entry["id"] for entry in self.m_api_object["characters"]]
         
-    def to_json(self):
+    def save_as_json(self):
 
-        return {
+        self.m_json_object = {
 
             "id": self.m_api_object["id"],
             "label": self.m_api_object["label"],
@@ -248,7 +348,7 @@ def build_site_json_files(p_grouping_ids, p_output_directory):
     character_ids = []
     character_objects = []
     grouping_objects = []
-    for grouping_id in p_grouping_ids:
+    for grouping_id in tqdm(p_grouping_ids, desc="Groupings"):
 
         # A. Get the grouping object from the database
         grouping_obj = Grouping(grouping_id)
@@ -257,13 +357,23 @@ def build_site_json_files(p_grouping_ids, p_output_directory):
         # B.Accrue a list of characters
         character_ids.extend(grouping_obj.characters)
 
+        # break
+
     # 2. Create a list of character objects from the database
     character_ids = list(set(character_ids))
-    character_objects = [Character(character_id) for character_id in character_ids]
+    character_objects = [Character(character_id) for character_id in tqdm(character_ids, desc="Characters")]
+    # character_objects = []
+    # for id in character_ids:
+    #     character_objects.append(Character(id))
+    #     break
 
     # 3. Create a list of book objects where the characters are featured
     book_ids = list(set([character.book for character in character_objects]))
-    book_objects = [Book(book_id) for book_id in book_ids]
+    book_objects = [Book(book_id) for book_id in tqdm(book_ids, desc="Books")]
+    # book_objects = []
+    # for id in book_ids:
+    #     book_objects.append(Book(id))
+    #     break
 
     print(f"Writing json data for catalogue to subdirectories in {p_output_directory}...")
 
@@ -283,7 +393,7 @@ def build_site_json_files(p_grouping_ids, p_output_directory):
 
         # B. Write out all json objects to file in this subdirectory
         for obj in object_info[subdir]:
-            json_data_str = json.dumps(obj.to_json(), indent=4)
+            json_data_str = json.dumps(obj.json_object, indent=4)
 
             output_filename = f"{p_output_directory}{subdir}{os.sep}{obj.id}.json"
             print(f"Writing {output_filename}...")
@@ -294,25 +404,43 @@ def build_site_json_files(p_grouping_ids, p_output_directory):
 
 def create_site_images(p_book_objects, p_character_objects, p_grouping_objects, p_image_output_directory):
 
-    # 1. Create paths in the image output directory
+    # 1. Create all image paths (for images currently used by the site) in the image output directory
+
+    print("Creating image directories...")
     
     # A. Book object paths
+    for book_object in tqdm(p_book_objects, desc="Directories for book images"):
+        book_object.make_image_paths(p_image_output_directory)
 
-    for book_object in p_book_objects:
-        
-        # I. 'IIIF base' path
-        # (ex. /img/iiif/books/british_library/asowle_xxxxx_yyyyy_00height_heathen/lines_color/asowle_xxxxx_yyyyy_00height_heathen-0001_page1r.tif)
-        # NOTE: This will also create the parent path /img/iiif/books/ if it does not yet exist in the output directory
-        os.makedirs(p_image_output_directory + book_object["iiifBase"])
-
-        # II. 'Web URL' path
-        os.makedirs(os.path.basename(p_image_output_directory + book_object["webUrl"]))
-
-        # III. 'Thumbnail' path
-        os.makedirs(os.path.basename(p_image_output_directory + book_object["thumbnail"]))
-
+    # B. Character object paths
+    for character_object in tqdm(p_character_objects, desc="Directories for character images"):
+        character_object.make_image_paths(p_image_output_directory)
 
     # 2. Download full page images to the appropriate subdirectories
+
+    print(f"Downloading images...")
+
+    # A. Get list of all image files to download
+    base_images_to_download = []
+    base_images_to_download = [book_object.json_object["coverPage"]["image"]["thumbnail"] for book_object in p_book_objects]
+    base_images_to_download.extend([character_object.json_object["page"]["image"]["thumbnail"] for character_object in p_character_objects])
+    base_images_to_download = list(set(base_images_to_download))
+
+    # B. Download images
+    for image_path in tqdm(base_images_to_download, desc="Images"):
+        API_Object.get_workbench_image(image_path, "/img/", p_image_output_directory)        
+
+    # B. Remove workbench url prefix from paths
+
+    # Book
+    # ["coverPage"]["image"]["iiifBase"]: "/img/iiif/books/british_library/asowle_xxxxx_yyyyy_00height_heathen/lines_color/asowle_xxxxx_yyyyy_00height_heathen-0001_page1r.tif"
+    # ["coverPage"]["image"]["webUrl"]: "/img/iiif/books/british_library/asowle_xxxxx_yyyyy_00height_heathen/lines_color/asowle_xxxxx_yyyyy_00height_heathen-0001_page1r.tif/full/full/0/default.jpg"
+    # ["coverPage"]["image"]["thumbnail"]: "/img/iiif/books/british_library/asowle_xxxxx_yyyyy_00height_heathen/lines_color/asowle_xxxxx_yyyyy_00height_heathen-0001_page1r.tif/full/200,/0/default.jpg"
+
+    # Character
+    # ["image"]["buffer"]: "/img/iiif/books/restoration/anon_R5466_usnnnc_menetekel1663/lines_color/anon_R5466_usnnnc_menetekel1663-0005_page1r.tif/1477,1055,179,169/150,/0/default.jpg"
+    # ["image"]["webUrl"]: "/img/iiif/books/restoration/anon_R5466_usnnnc_menetekel1663/lines_color/anon_R5466_usnnnc_menetekel1663-0005_page1r.tif/1527,1105,79,69/full/0/default.jpg"
+    # ["page"]["image"]["iiifBase"]: "/img/iiif/books/restoration/anon_R5466_usnnnc_menetekel1663/lines_color/anon_R5466_usnnnc_menetekel1663-0005_page1r.tif"
 
     # 3. Extract the needed sub-images and scaled images from the full images
     #    and put them in the appropriate subdirectories
