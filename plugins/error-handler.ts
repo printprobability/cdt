7e9b2bbbfca6bbab79eacd7e1164804221ed3b60
Henry P. <hungphamlk@gmail.com>
2024-10-05 23:22:22 +0700

    Update error-handler.ts

--- plugins/error-handler.ts
+++ plugins/error-handler.ts
@@ -1,22 +1,26 @@
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
 
-      // handle error, e.g. report to a service
-      console.error(error)
-      console.log("======================================");
-      console.log("errorHandler");
-      console.log(`error: ${JSON.stringify(error)}`);
-      console.log(`info: ${info}`);
+      if (process.env.APP_ENV !== 'production') {
+        // handle error, e.g. report to a service
+        console.error(error)
+        console.log("======================================");
+        console.log("errorHandler");
+        console.log(`error: ${JSON.stringify(error)}`);
+        console.log(`info: ${info}`);
+      }
     }
 
     // Also possible
     nuxtApp.hook('vue:error', (error, instance, info) => {
 
-      // handle error, e.g. report to a service
-      console.error(error)
-      console.log("======================================");
-      console.log("vue:error");
-      console.log(`error: ${JSON.stringify(error)}`);
-      console.log(`info: ${info}`);
+      if (process.env.APP_ENV !== 'production') {
+        // handle error, e.g. report to a service
+        console.error(error)
+        console.log("======================================");
+        console.log("vue:error");
+        console.log(`error: ${JSON.stringify(error)}`);
+        console.log(`info: ${info}`);
+      }
     })
   })
